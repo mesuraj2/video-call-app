640e5e8687c268af303450d9301b02f0ecaaea8e
Suraj Kumar <znsuraj7@gmail.com>
2022-09-02 21:19:26 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
-import Navbar from './Components/Navbar';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import React from "react";
+import Home from "./Components/Home";
+import About from "./Components/about";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <h1 className='bg-slate-500'>hello world</h1>
-    </>
+    <Router>
+      <h1>hello world suraj</h1>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+ 
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div>
+      <h1>hello it is my about page</h1>
+    </div>
+  )
+}
