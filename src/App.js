78fa607d9351c3da176fa833174882a4ce1de84f
Suraj Kumar <znsuraj7@gmail.com>
2022-09-02 19:16:21 +0530

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          welcome to video call app by ai student sai pradeep
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <h1 className='bg-slate-500'>hello world</h1>
+    </>
   );
 }
 
---
+++ src/Components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function footer() {
+  return (
+    <div>
+      <h1>hello it is footer</h1>
+    </div>
+  )
+}
